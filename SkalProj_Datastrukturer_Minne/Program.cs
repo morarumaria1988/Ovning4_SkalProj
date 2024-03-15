6d872ad70206e945db04885738c615133ad7ce58
Maria Moraru <morarumaria1988@gmail.com>
2024-03-15 15:31:55 +0100

    1

--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -5,7 +5,7 @@ namespace SkalProj_Datastrukturer_Minne
     class Program
     {
         /// <summary>
-        /// The main method, vill handle the menues for the program test
+        /// The main method, will handle the menues for the program
         /// </summary>
         /// <param name="args"></param>
         static void Main()
@@ -34,15 +34,15 @@ namespace SkalProj_Datastrukturer_Minne
                     case '1':
                         ExamineList();
                         break;
-                    case '2':
-                        ExamineQueue();
-                        break;
-                    case '3':
-                        ExamineStack();
-                        break;
-                    case '4':
-                        CheckParanthesis();
-                        break;
+                    //case '2':
+                    //    ExamineQueue();
+                    //    break;
+                    //case '3':
+                    //    ExamineStack();
+                    //    break;
+                    //case '4':
+                    //    CheckParanthesis();
+                    //    break;
                     /*
                      * Extend the menu to include the recursive 
                      * and iterative exercises.
@@ -71,13 +71,48 @@ namespace SkalProj_Datastrukturer_Minne
              * As a default case, tell them to use only + or -
              * Below you can see some inspirational code to begin working.
             */
-
-            //List<string> theList = new List<string>();
-            //string input = Console.ReadLine();
-            //char nav = input[0];
-            //string value = input.substring(1);
-
-            //switch(nav){...}
+            bool examineList = true;
+            List<string> theList = new List<string>();
+            Console.WriteLine("Write something with + as prefix to add it to the list, with - to remove it or only 0 to return to the main menu.");
+            while (examineList)
+            {
+                Console.WriteLine("Listans kapacitet är just nu: " + theList.Capacity);
+                Console.WriteLine("Listans storlek är just nu: " + theList.Count);
+                string input = Console.ReadLine()!;
+                char nav = input[0];
+                string value = input.Substring(1);
+                switch (nav)
+                {
+                    case '+':
+                        theList.Add(value);
+                        break;
+                    case '-':
+                        theList.Remove(value);
+                        break;
+                    case '0':
+                        examineList = false;
+                        break;
+                    default:
+                        Console.WriteLine("Use only + or - in front!");
+                        break;
+                }
+            }
+            /*
+2. När ökar listans kapacitet? (Alltså den underliggande arrayens storlek)
+När listans storlek överskrider listans kapacitet.
+3. Med hur mycket ökar kapaciteten?
+Med dubbelt så mycket kapacitet som finns. Initialt med 4 om man inte angav en initial kapacitet i listans konstruktör.
+4. Varför ökar inte listans kapacitet i samma takt som element läggs till?
+För att balansera prestanda och minnesanvändning genom att minska antalet reallokeringar och kopieringar.
+När listan når sin nuvarande kapacitet, skapas en ny intern array med dubbelt så stor kapacitet. De befintliga elementen kopieras till den nya arrayen, och det nya elementet läggs till.
+Detta gör att listan kan hantera en ökande mängd data effektivt utan att behöva reallokera minne för varje enskilt element. 
+5. Minskar kapaciteten när element tas bort ur listan?
+Nej.
+6. När är det då fördelaktigt att använda en egendefinierad array istället för en lista?
+•	När man ska ha en fast kapacitet och vet exakt hur stor den ska vara.
+•	Om man har mycket stora datamängder och prestanda är avgörande.  Direkt åtkomst till element i en array är snabbare eftersom det inte finns någon overhead för metoder som Add, Remove, etc. 
+•	Om man behöver finjustera minnesanvändningen och hantera minnet manuellt. Man har full kontroll över minnesallokering och frigöring.
+             */
         }
 
         /// <summary>
